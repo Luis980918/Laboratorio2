f0c215f3d895e7e5a162df67d62a721507b98a85
Luis Carlos <lcarlos.rendon@udea.edu.co>
2021-04-21 23:56:10 -0500

    Halow remastered

--- Shell.c
+++ Shell.c
@@ -8,11 +8,31 @@
 int main(){
     
     char b[20];
-
+    char *delim;
     do{
 
      printf("wish> ");
      scanf("%s",b);
+    printf("%c\n",b[strlen(b)-1]);
+    if(b[strlen(b)+1]==' '){
+        b[strlen(b)+1]=',';
+        //delim=strtok(b, '\0');
+        printf("%s\n", b);
+        if(strcmp(b, "cd")==0){
+         printf("Ejecutando cd\n");
+        }else{
+            if(strcmp(b, "ls")==0){
+            printf("Ejecutando ls\n");
+            }else{
+                if(strcmp(b, "path")==0){
+                printf("Ejecutando path\n");
+                }else{
+                    printf("Comando erroneo, verifique en la ruta path\n");
+                }
+            }
+        }
+    }
+
     }while(strcmp(b, "exit")!=0);
     return(0);
 }
