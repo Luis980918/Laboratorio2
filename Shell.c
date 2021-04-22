effc85f3d512ead2a5674770c9c9735528902d05
Luis Carlos <lcarlos.rendon@udea.edu.co>
2021-04-21 21:57:02 -0500

    Halow

--- Shell.c
+++ Shell.c
@@ -5,40 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 
-
 int main(){
-    int pid;
-    int cerrar = 1;
-    char * lectura;
-
-    /*printf("PADRE: Soy el proceso padre y mi pid es: %d\n", getpid());
-    pid = fork();
     
+    char b[20];
 
-    // En cuanto llamamos a fork se crea un nuevo proceso. En el proceso
-    // padre 'pid' contendrá el pid del proceso hijo. En el proceso hijo
-    // 'pid' valdrá 0. Eso es lo que usamos para distinguir si el código
-    // que se está ejecutando pertenece al padre o al hijo.
+    do{
 
-    if (pid) // Este es el proceso padre
-    {
-        printf("PADRE: Soy el proceso padre y mi pid sigue siendo: %d\n", getpid());
-        printf("PADRE: Mi hijo tiene el pid: %d\n", pid);
-    }
-    else // Proceso hijo
-    {
-        printf("HIJO: Soy el proceso hijo y mi pid es: %d\n", getpid());
-        printf("HIJO: mi padre tiene el pid: %d\n", getpid());
-    }*/
-
-    while (cerrar != 0){
-        printf("Wish\n");
-        scanf("%s", lectura);
-        if(lectura == 0){
-         cerrar = 0;
-        }
-       
-    }
-
-
+     printf("wish> ");
+     scanf("%s",b);
+    }while(strcmp(b, "exit")!=0);
+    return(0);
 }
